47404279b099c5f050bc49afc5130b31301e210e
肖冬丹 <dongdan.xiao@chinaredstar.com>
2017-10-23 10:48:26 +0800

    Initial Commit

---
+++ Sensor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sensor
+//
+//  Created by xiaodongdan on 2017/10/23.
+//  Copyright © 2017年 xiaodongdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
